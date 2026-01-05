50656eec7601f013dfb86b739c516648acf14eb5
alamdarhussain-eng <alamdar.hussain@seecs.edu.pk>
2026-01-05 15:11:37 +0500

    Create MainActivity.java

---
+++ app/src/main/app/src/main/java/com/example/weeklyschedule/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.weeklyschedule;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        // ðŸ”´ Ø§Ù¾Ù†Ø§ Google Apps Script Web App URL ÛŒÛØ§Úº Ù„Ú¯Ø§Ø¦ÛŒÚº
+        webView.loadUrl("https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
